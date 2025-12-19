c2585a6e5b06cc25b9297812829238667714fdc5
Ankit Kumar <ankitkumar61586603@gmail.com>
2025-12-19 20:46:06 +0530

    Delete state.py

--- state.py
+++
@@ -1,8 +0,0 @@
-from typing import TypedDict, List
-
-class AgentState(TypedDict):
-    topic: str                
-    summary_length: str       # <--- NEW FIELD
-    research_plan: List[str]  
-    search_results: str       
-    final_report: str         
